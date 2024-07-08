ef4f5f86561bcf693578411fdf066a2c756e29b2
Pourya <pourya.nejadensan@gmail.com>
2024-07-08 15:53:44 +0200

    Step 1: Create a class 'Medication' with the methods 'getName', 'getPrice', and 'getAvailability'.

---
+++ src/main/java/Medication.java
@@ -0,0 +1,17 @@
+public class Medication {
+    private String name;
+    private double price;
+    private int availability;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+}
